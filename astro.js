b0d10353d4deb517c3d66679dd184520eeae86c9
Nayanp35 <prajapatinayan35@gmail.com>
2023-02-28 12:10:51 -0500

    created start scene

--- astro.js
+++ astro.js
@@ -171,7 +171,16 @@ loadSprite("pipe", "image/newPipe.png");
 
 let highScore = 0;
 
+scene("start", () => {
+	add([
+        text("Press space to start"),
+    ]);
+	onKeyPress("space", () => go("game"));
+})
+
+
 scene("game", () => {
+	character.style.display = "none";
   const PIPE_GAP = 200;
   let score = 0;
 
@@ -180,7 +189,7 @@ scene("game", () => {
   ]);
 
   const scoreText = add([
-    text(score, {size: 50})
+    text(`Score: ${score}`, {size: 50})
   ]);
 
   // add a game object to screen
@@ -225,7 +234,7 @@ scene("game", () => {
     if (pipe.passed === false && pipe.pos.x < player.pos.x) {
       pipe.passed = true;
       score += 1;
-      scoreText.text = score;
+      scoreText.text = `Score: ${score}`;
     }
   });
 
@@ -264,4 +273,4 @@ scene("gameover", (score) => {
   });
 });
 
-go("game");
+go("start"); 
